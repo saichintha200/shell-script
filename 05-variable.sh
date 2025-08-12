7e40ebb1feef97d5c1c61d8c9e5517087d41d73c
saichintha200 <saichintha200@gamil.com>
2025-08-12 22:55:12 +0530

    msg

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,8 +1,10 @@
 #1/bin/bash
 
 echo "please enter the username::"
-read USERNAME # here USERNAME is variable
+read -s USERNAME # here USERNAME is variable
 echo "pelase enter the password"
-read PASSWORD
+read -s PASSWORD
 
 echo "Username is $USERNAME and password is $PASSWORD"
+
+#if we use -s then to hide the data
