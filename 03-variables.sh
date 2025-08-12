eee5c9fcdd5b117be1dcbcdca8681fda35c0e6ce
saichintha200 <saichintha200@gamil.com>
2025-08-12 22:37:07 +0530

    msg

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=ramesh
+PERSON2=sai
+
+#referring variable
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "${PERSON2}:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am fine too. how is your work?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
