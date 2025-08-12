bf62e2c4e11db88922fba2f280f3d655aedd26d2
saichintha200 <saichintha200@gamil.com>
2025-08-12 23:22:51 +0530

    msg

--- 08-specail-variables.sh
+++ 08-specail-variables.sh
@@ -1,3 +1,6 @@
 echo "All variables: $@"
 echo "Number of variables passed: $#"
 echo "Script Name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
