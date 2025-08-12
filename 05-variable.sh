c29be9636ba58e922cc568f0e8cbb01eaa8fd032
saichintha200 <saichintha200@gamil.com>
2025-08-12 22:53:51 +0530

    msg

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,8 +1,8 @@
 #1/bin/bash
 
-echo"please enter the username::"
+echo "please enter the username::"
 read USERNAME # here USERNAME is variable
-echo"pelase enter the password"
+echo "pelase enter the password"
 read PASSWORD
 
 echo "Username is $USERNAME and password is $PASSWORD"
