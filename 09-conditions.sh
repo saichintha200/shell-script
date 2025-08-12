5d43a234d802eb9f8c620b5d63dbe21217bab7d3
saichintha200 <saichintha200@gamil.com>
2025-08-12 23:45:16 +0530

    msg

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 NUMBER=$1
-if [$NUMBER -gt 10]
+if [ $NUMBER -gt 10 ] 
 then
-echo "Givne number $NUMBER is greathan 10"
+   echo "Givne number $NUMBER is greathan 10"
 else
-echo "Given number $NUMBER2 is less than 10"
+   echo "Given number $NUMBER2 is less than 10"
 fi
