d40e078ad1a97b13a23feb86f51082917a499b95
saichintha200 <saichintha200@gamil.com>
2025-08-13 19:33:22 +0530

    msg

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -7,3 +7,4 @@ then
 else
    echo "Given number $NUMBER is less than 10"
 fi
+#-gt, -lt, -eq, -ge, -le
---
+++ 10-installation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [$USERID -ne 0]
+
+then 
+   echo  "Please run this command using root access"
+
+else
+   echo "You are super user"
+fi
+
+dnf install mysql -y
+
+echo "use exit code"
